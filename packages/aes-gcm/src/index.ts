5c4a4043306a00cb26f4b461b62e2bfbde86bc77
lightsing <light.tsing@gmail.com>
2022-12-08 00:11:54 +0800

    add aes-128-gcm

--- packages/aes-gcm/src/index.ts
+++ packages/aes-gcm/src/index.ts
@@ -1,16 +1,23 @@
-import type { AeadContext } from '@web3-social/hpke-framework';
+import type { AeadContext } from '@web3-social/hpke-framework'
 import { getCrypto } from '@web3-social/hpke-framework'
 
-const crypto = getCrypto();
+const crypto = getCrypto()
 
-export class Aes256GcmAead implements AeadContext {
+abstract class AesGcmAead implements AeadContext {
     /** The length in bytes of a key for this algorithm. */
-    public readonly nK: number = 32;
+    public readonly nK: number
     /** The length in bytes of a nonce for this algorithm. */
-    public readonly nN: number = 12;
+    public readonly nN: number
     /** The length in bytes of the authentication tag for this algorithm. */
-    public readonly nT: number = 16;
-    public readonly aeadId: number = 0x0002;
+    public readonly nT: number
+    public readonly aeadId: number
+
+    protected constructor(nK: number, nN: number, nT: number, aeadId: number) {
+        this.nK = nK
+        this.nN = nN
+        this.nT = nT
+        this.aeadId = aeadId
+    }
 
     /**
      * Encrypt and authenticate plaintext pt with associated data aad
@@ -24,14 +31,14 @@ export class Aes256GcmAead implements AeadContext {
      * @throws MessageLimitReachedError
      */
     public async seal(key: Uint8Array, nonce: Uint8Array, pt: Uint8Array, aad?: Uint8Array): Promise<Uint8Array> {
-        const importKey = await crypto.subtle.importKey('raw', key, 'AES-GCM', false, ['encrypt']);
+        const importKey = await crypto.subtle.importKey('raw', key, 'AES-GCM', false, ['encrypt'])
         const alg = {
             name: 'AES-GCM',
             iv: nonce,
-            additionalData: aad
+            additionalData: aad,
         }
-        const ct = await crypto.subtle.encrypt(alg, importKey, pt);
-        return new Uint8Array(ct);
+        const ct = await crypto.subtle.encrypt(alg, importKey, pt)
+        return new Uint8Array(ct)
     }
 
     /**
@@ -47,13 +54,25 @@ export class Aes256GcmAead implements AeadContext {
      * @throws MessageLimitReachedError
      */
     public async open(key: Uint8Array, nonce: Uint8Array, ct: Uint8Array, aad?: Uint8Array): Promise<Uint8Array> {
-        const importKey = await crypto.subtle.importKey('raw', key, 'AES-GCM', false, ['decrypt']);
+        const importKey = await crypto.subtle.importKey('raw', key, 'AES-GCM', false, ['decrypt'])
         const alg = {
             name: 'AES-GCM',
             iv: nonce,
-            additionalData: aad
-        };
-        const pt = await crypto.subtle.decrypt(alg, importKey, ct);
-        return new Uint8Array(pt);
+            additionalData: aad,
+        }
+        const pt = await crypto.subtle.decrypt(alg, importKey, ct)
+        return new Uint8Array(pt)
+    }
+}
+
+export class Aes128GcmAead extends AesGcmAead {
+    constructor() {
+        super(16, 12, 16, 0x0001)
+    }
+}
+
+export class Aes256GcmAead extends AesGcmAead {
+    constructor() {
+        super(32, 12, 16, 0x0002)
     }
 }
--- packages/integration-tests/tests/basic.test.ts
+++ packages/integration-tests/tests/basic.test.ts
@@ -1,35 +1,60 @@
-import { assert, describe, it } from 'vitest'
-import {CipherSuite, HpkeMode, XCryptoKey} from "@web3-social/hpke-framework";
-import {Secp256k1Blake3Kem} from "@web3-social/hpke-secp256k1-blake3";
-import {HkdfBlake3Factory} from "@web3-social/hpke-hkdf-blake3";
-import {Aes256GcmAead} from "@web3-social/hpke-aes-gcm";
+import { describe, expect, it } from 'vitest'
+import { CipherSuite, HpkeMode, XCryptoKey, getCrypto } from '@web3-social/hpke-framework'
+import { Secp256k1Blake3Kem } from '@web3-social/hpke-secp256k1-blake3'
+import { HkdfBlake3Factory } from '@web3-social/hpke-hkdf-blake3'
+import { Aes128GcmAead, Aes256GcmAead } from '@web3-social/hpke-aes-gcm'
 
-describe('cipher suite', () => {
-  it('basic', async () => {
-    const cipherSuite = new CipherSuite({
-      kem: new Secp256k1Blake3Kem(),
-      kdfFactory: HkdfBlake3Factory,
-      aead: new Aes256GcmAead(),
-    });
-  
-    // const { privateKey: skS, publicKey: pkS } = await cipherSuite.kem.generateKeyPair();
-    const { privateKey: skR, publicKey: pkR } = await cipherSuite.kem.generateKeyPair();
-  
-    const { enc, ctx: contextS} = await cipherSuite.createSenderContext({
-      mode: HpkeMode.Base,
-      pkR: pkR as XCryptoKey
-    });
-  
-    const ct = await contextS.seal(new TextEncoder().encode('test message'));
-  
-    const contextR = await cipherSuite.createReceiverContext({
-      mode: HpkeMode.Base,
-      enc,
-      skR: skR as XCryptoKey,
-    });
-  
-    const pt = await contextR.open(ct);
-    console.log(new TextDecoder().decode(pt));
-  })
-  
-});
+const crypto = getCrypto()
+
+describe('cipher suite base mode', () => {
+    const baseTest = async (cipherSuite: CipherSuite) => {
+        const { privateKey: skR, publicKey: pkR } = await cipherSuite.kem.generateKeyPair()
+
+        const randomSize = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min
+
+        for (let i = 0; i < 10; i++) {
+            const { enc, ctx: contextS } = await cipherSuite.createSenderContext({
+                mode: HpkeMode.Base,
+                pkR: pkR as XCryptoKey,
+            })
+
+            const PT = [...Array(10).keys()].map(() => {
+                const pt = new Uint8Array(randomSize(1, 1024))
+                crypto.getRandomValues(pt)
+                return pt
+            })
+
+            const ct = await Promise.all(PT.map((pt) => contextS.seal(pt)))
+
+            const contextR = await cipherSuite.createReceiverContext({
+                mode: HpkeMode.Base,
+                enc,
+                skR: skR as XCryptoKey,
+            })
+
+            const pt = await Promise.all(ct.map((ct) => contextR.open(ct)))
+
+            expect(pt).toEqual(PT)
+        }
+    }
+
+    it('aes128', async () => {
+        await baseTest(
+            new CipherSuite({
+                kem: new Secp256k1Blake3Kem(),
+                kdfFactory: HkdfBlake3Factory,
+                aead: new Aes128GcmAead(),
+            }),
+        )
+    })
+
+    it('aes256', async () => {
+        await baseTest(
+            new CipherSuite({
+                kem: new Secp256k1Blake3Kem(),
+                kdfFactory: HkdfBlake3Factory,
+                aead: new Aes256GcmAead(),
+            }),
+        )
+    })
+})
